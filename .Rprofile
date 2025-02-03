f6f7b571acfe45c5ca4ff5383406485d24db21f8
Cindy Hu <xindy.hu@gmail.com>
2025-02-03 16:49:30 -0500

    feat: develop code for hyperparameter tuning, prepare for pegasus

--- .Rprofile
+++ .Rprofile
@@ -1,1 +1,3 @@
 source("renv/activate.R")
+Sys.setenv(PROJ_LIB = "/opt/homebrew/share/proj")  # Adjust the path as needed
+
--- R/5_Regression_Model.R
+++ R/5_Regression_Model.R
@@ -16,10 +16,10 @@ names(metals) <- metal.codes
 
 tune_xgboost_models <- function(metal.code) {
   ### 1. set up and split data randomlly -----------------------------------------------------------------------------------------------------------
-  df <- readRDS(paste0("R_Output/", metal.code, "_df_PredictorsSelected.rds")) #%>%
-  #   group_by(.imp) %>%
-  #   sample_n(size = 500, replace = FALSE) %>%
-  #   ungroup()
+  df <- readRDS(paste0("R_Output/", metal.code, "_df_PredictorsSelected.rds")) %>%
+    group_by(.imp) %>%
+    sample_n(size = 500, replace = FALSE) %>%
+    ungroup()
   df$logconc = log10(df$conc)
   df$is.imputed = as.integer(df$censored)
   
@@ -40,6 +40,7 @@ tune_xgboost_models <- function(metal.code) {
         date,
         conc,
         DL.missing,
+        detect.limit,
         censored,
         is.imputed,
         data.source,
@@ -52,7 +53,7 @@ tune_xgboost_models <- function(metal.code) {
       ) %>%
       recipes::step_zv(all_predictors()) %>%
       recipes::step_normalize(all_numeric_predictors()) %>%
-      recipes::step_dummy(all_nominal_predictors())
+      recipes::step_dummy(all_nominal_predictors()) 
   }
   
   # Apply preprocessing to each imputed dataset
@@ -68,9 +69,9 @@ tune_xgboost_models <- function(metal.code) {
       loss_reduction = tune()
     ) %>%
     set_engine('xgboost') %>%
-    set_mode('regression')
-  
-  
+    parsnip::set_mode('regression')
+
+
   # Compile workflow for each imputed dataset
   model_workflows <- map(
     train_recipes,
@@ -78,22 +79,22 @@ tune_xgboost_models <- function(metal.code) {
       workflows::add_recipe(.x) %>%
       workflows::add_model(tree_model)
   )
-  
-  ### 3. Conduct parameter tuning with cross-validation ------------------------------------------------------------------------------------
+
+  #### 3. Conduct parameter tuning with cross-validation ------------------------------------------------------------------------------------
   # Set up cross-validation for each imputed dataset
   cv_folds <- map(df_train, ~ vfold_cv(.x, v = 10, repeats = 1))
-  
+
   # # Define parameter grid
   model_grid <- expand.grid(
     trees = c(100, 200, 500),
-    tree_depth = c(6),#, 9, 12),
-    learn_rate = c(0.01),#, 0.03, 0.05),
-    sample_size = c(0.6),#, 0.8, 1.0),
-    loss_reduction = c(0)#, 0.5, 1, 5)
+    tree_depth = c(6, 9, 12),
+    learn_rate = c(0.01, 0.03, 0.05),
+    sample_size = c(0.6, 0.8, 1.0),
+    loss_reduction = c(0, 0.5, 1, 5)
   )
-  
+
   # Perform hyperparameter tuning for each imputation
-  tune_results <- map2(
+  system.time({tune_results <- map2(
     model_workflows,
     cv_folds,
     ~ tune_grid(
@@ -102,41 +103,44 @@ tune_xgboost_models <- function(metal.code) {
       grid = model_grid,
       metrics = metric_set(rsq, rmse, mae)
     )
-  )
-  
+  )})
+
   # Collect tuning results for all imputations
-  all_tune_results <- map_dfr(tune_results, collect_metrics, .id = "imputation")
-  
-  # Print summarized results
-  tune_summary <- all_tune_results %>%
-    group_by(.metric, imputation) %>%
+  all_tune_results <- map_dfr(tune_results, collect_metrics, .id = "imputation") %>%
+    # average across five imputations to find the best hyperparameters
+    group_by(.metric, .config) %>%
     summarise(
       mean_val = mean(mean),
-      sd_val = sd(mean),
-      min_val = min(mean),
-      max_val = max(mean),
-      min_p_sd = sd_val + min_val,
-      .groups = 'drop'
+      # pooled std_err, same n, sum of squares divided by the number of configurations, and then square root
+      mean_p_sd = sqrt(sum(std_err^2) / n()),
+      # same configuration has the same hyperparameters
+      across(c(trees, tree_depth, learn_rate, sample_size, loss_reduction, n), ~ first(.x)),
+      .groups = 'keep'
     )
+  
+  tune_summary <- all_tune_results %>%
+    group_by(.metric) %>%
+    summarise(min_val = min(mean_val), 
+              pooled_sd = sqrt(sum(mean_p_sd^2) / n())) %>%
+    mutate(min_plus_sd = min_val + pooled_sd)
   
   ## 6. select best and simplest models from grid search -------------------------------------------------------------------------------------------------------------
   ## select all models within 1 SE of the best RMSE
   good_models <- all_tune_results %>%
     filter(.metric == 'rmse') %>%
     left_join(
-      tune_summary %>% select(.metric, imputation, min_val, min_p_sd),
-      by = c('imputation', '.metric')
+      tune_summary %>% select(.metric, min_val, min_plus_sd),
+      by = c('.metric')
     ) %>%
-    filter(mean < min_p_sd, mean > min_val)
-  
-  if (nrow(good_models) > n_distinct(df$.imp)) {
+    filter(mean_val < min_plus_sd, mean_val > min_val)
+
+  if (nrow(good_models) > 1) {
     good_models <- good_models %>%
-      group_by(imputation) %>%
       arrange(trees, tree_depth, sample_size, desc(learn_rate)) %>%
       filter(row_number() == 1)
   }
-  
-  
+
+
   write_csv(good_models,
             paste0("R_Output/", metal.code, "_df_hyperparameter.csv"))
   print(paste0('Variable selection for ', metal.code, ' complete'))
@@ -145,6 +149,47 @@ tune_xgboost_models <- function(metal.code) {
 
 purrr::map(metal.codes, tune_xgboost_models)
 
+# xgboost_without_tuning <- function(i){
+#   # Define the XGBoost model with fixed hyperparameters
+#   xgb_spec <- boost_tree(
+#     trees = 500,           # Fixed number of trees
+#     tree_depth = 9,        # Fixed tree depth
+#     learn_rate = 0.03,     # Fixed learning rate
+#     loss_reduction = 0, # Minimum loss reduction
+#     sample_size = 0.6,     # Row sampling
+#     mtry = floor(0.6 * ncol(df_train[[i]])-1),             # Feature sampling
+#     mode = "regression"    # Change to "classification" if needed
+#   ) %>%
+#     set_engine("xgboost")
+#   
+#   # Create a workflow
+#   xgb_wf <- workflow() %>%
+#     add_recipe(train_recipes[[i]]) %>%
+#     add_model(xgb_spec)
+#   
+#   # Fit the model to the training data
+#   xgb_fit <- fit(xgb_wf, data = df_train[[i]])
+#   
+#   # Make predictions on test data
+#   predictions <- predict(xgb_fit, df_test[[i]]) %>%
+#     bind_cols(df_test[[i]])
+#   
+#   # Evaluate model performance
+#   metrics <- predictions %>%
+#     metrics(truth = logconc, estimate = .pred)
+#   
+#   # Print evaluation metrics
+#   print(metrics)
+#   
+#   # Plot variable importance
+#   xgb_fit %>%
+#     extract_fit_parsnip() %>%
+#     vip::vip(num_features = 10) +
+#     ggtitle(paste0("Variable Importance for ", metal.code, " Prediction, Imputation [", i, ']'))
+#   
+# }
+# 
+# purrr::map(1:5, xgboost_without_tuning)
 # ## hyperparameters for 'best selected' model from good_models (best judgement from previous step)
 # df_hp <- data.frame(metals=c('As','Mn','Sr','Li','Cd'),
 #                     trees=c(500, 500, 500, 100, 100),
@@ -152,12 +197,12 @@ purrr::map(metal.codes, tune_xgboost_models)
 #                     learn_rate=c(0.03, 0.01, 0.03, 0.05, 0.05),
 #                     mtry=c(0.6, 0.6, 0.6, 0.6, 0.6),
 #                     sample_size=c(0.8, 0.8, 0.6, 1.0, 0.6),
-#                     loss_red=c(1.0, 1.0, 0, 0.5, 5))
+#                     loss_reduction=c(1.0, 1.0, 0, 0.5, 5))
 # df_hp <- df_hp %>% column_to_rownames(., var='metals')
-
+# 
 # #### 5. update model workflow -------------------------------------------------------------------------------------
 # # Update model workflow with best selected hyperparameters
-# best.model.workflow <- model.workflow %>%
+# best_model_workflow <- model_workflows[[1]] %>%
 #   update_model(
 #     boost_tree(
 #       trees = df_hp[metal.code, 'trees'],
@@ -165,11 +210,11 @@ purrr::map(metal.codes, tune_xgboost_models)
 #       learn_rate = df_hp[metal.code, 'learn_rate'],
 #       mtry = df_hp[metal.code, 'mtry'],
 #       sample_size = df_hp[metal.code, 'sample_size'],
-#       loss_red = df_hp[metal.code, 'loss_red'],
+#       loss_reduction = df_hp[metal.code, 'loss_reduction'],
 #       engine = 'xgboost'
 #     )
 #   )
-#
+
 #
 # ### 6. train final models -------------------------------------------------------------------------------------------------------------
 # set.seed(123)
